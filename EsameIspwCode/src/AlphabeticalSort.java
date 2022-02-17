0f9fe6558dcca319a1240209fcb6a7361020e640
alessandrocortese <74416238+alessandro-cortese@users.noreply.github.com>
2022-02-17 11:24:35 +0100

    Add code for alphabetic sort of names

---
+++ EsameIspwCode/src/AlphabeticalSort.java
@@ -0,0 +1,27 @@
+public class AlphabeticalSort {
+
+    public static void alphabeticSort(String[] names) {
+
+        String temp;
+        int i;
+        int j;
+
+        for(i = 0; i < names.length; i++) {
+
+            for(j = i + 1; j < names.length; j++) {
+
+                if(names[i].compareTo(names[j]) > 0) {
+
+                    temp = names[i];
+                    names[i] = names[j];
+                    names[j] = temp;
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
